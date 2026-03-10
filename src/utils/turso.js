11112fae6a6fe42548c1d594e074cfe29c619bd7
aida-martinez <aida-martinez@outlook.com>
2026-03-10 23:23:25 +0100

    Fix fetch illegal invocation in turso client

--- src/utils/turso.js
+++ src/utils/turso.js
@@ -6,4 +6,5 @@ const tursoAuthToken = import.meta.env.VITE_TURSO_AUTH_TOKEN;
 export const tursoClient = createClient({
   url: tursoUrl,
   authToken: tursoAuthToken,
+  fetch: (...args) => fetch(...args),
 });
